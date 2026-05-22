8ad6f698ee9c88fcda260b8f504800fd4eb4e2e7
Liam <lvanmeeretn56@gmail.com>
2026-05-22 12:41:25 -0700

    factchecking

--- src/main/java/frc/robot/subsystems/extender/extenderIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/extender/extenderIOTalonFX.java
@@ -7,5 +7,9 @@ import com.ctre.phoenix6.hardware.TalonFX;
 public class extenderIOTalonFX implements extenderIO {
   private final TalonFX extenderMotor =
       new TalonFX(EXTENDER_MOTOR.getDeviceNumber(), EXTENDER_MOTOR.getCANBus());
-      
+
+  @Override
+  public void stop() {
+    extenderMotor.stopMotor();
+  }
 }
